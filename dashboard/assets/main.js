af9c758c791ccb95999921afde1ddf905ff71057
Daniel <djperrefort@gmail.com>
2022-12-15 21:53:58 -0500

    Fixes bad auto formatting from JS IDE

--- dashboard/assets/main.js
+++ dashboard/assets/main.js
@@ -1,9 +1,9 @@
 // Dummy data for populating the webpage
-const dashboardVersion = "0.0.1"
-const termsContent = "This is the application license."
-const pipelineTitle = "My Awesome Pipeline"
-const pipelineID = "9ec463ad-db81-4ab2-bc9c-3ee3962ddb34"
-const pipelineDescription = "This is the description for a pipeline built using example data."
+const dashboardVersion = "0.0.1";
+const termsContent = "This is the application license.";
+const pipelineTitle = "My Awesome Pipeline";
+const pipelineID = "9ec463ad-db81-4ab2-bc9c-3ee3962ddb34";
+const pipelineDescription = "This is the description for a pipeline built using example data.";
 
 /**
  * Update dashboard content for the first time.
@@ -12,25 +12,25 @@ const pipelineDescription = "This is the description for a pipeline built using 
  * the dashboard template.
  */
 function setup() {
-    $(".eg-dashboard-version").map(function () {
-        this.textContent = dashboardVersion;
-    });
+  $(".eg-dashboard-version").map(function () {
+    this.textContent = dashboardVersion;
+  });
 
-    $(".eg-dashboard-terms").map(function () {
-        this.textContent = termsContent;
-    });
+  $(".eg-dashboard-terms").map(function () {
+    this.textContent = termsContent;
+  });
 
-    $(".eg-pipeline-title").map(function () {
-        this.textContent = pipelineTitle;
-    });
+  $(".eg-pipeline-title").map(function () {
+    this.textContent = pipelineTitle;
+  });
 
-    $(".eg-pipeline-id").map(function () {
-        this.textContent = pipelineID;
-    });
+  $(".eg-pipeline-id").map(function () {
+    this.textContent = pipelineID;
+  });
 
-    $(".eg-pipeline-description").map(function () {
-        this.textContent = pipelineDescription;
-    });
+  $(".eg-pipeline-description").map(function () {
+    this.textContent = pipelineDescription;
+  });
 }
 
 $(document).ready(setup);
