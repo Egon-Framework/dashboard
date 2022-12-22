0e48ec0c4e5144ee02f0e469e9e1146e5af2245f
Daniel <djperrefort@gmail.com>
2022-12-22 15:07:36 -0500

    Cleans up app.js file

--- src/app.js
+++ src/app.js
@@ -8,16 +8,18 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
-// view engine setup
+// Configure application logging
+app.use(logger("dev"));
+
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "public")));
-
+// Set up routing to application pages
 app.use("/", indexRouter);
+
+// Expose assets and static files
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use("/bootstrap-icons", express.static(__dirname + "/node_modules/bootstrap-icons/font"));
 
@@ -25,7 +27,7 @@ app.use("/bootstrap-icons", express.static(__dirname + "/node_modules/bootstrap-
 app.locals.title = "Egon Dashboard";
 app.locals.version = pjson.version;
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
