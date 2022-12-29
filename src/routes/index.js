e8cb5ee2fdaf7a1edd4281065d7f84ba141a220f
Daniel <djperrefort@gmail.com>
2022-12-29 11:33:56 -0500

    Renames index template to dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET home page
 router.get("/", function (req, res, next) {
-  res.render("index", {title: req.app.locals.title, version: req.app.locals.version});
+  res.render("dashboard", {title: req.app.locals.title, version: req.app.locals.version});
 });
 
 module.exports = router;
